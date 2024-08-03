290cca9e009cf0aeb3b261043c661e19658abe16
Kanykei <isakova0116@gmail.com>
2024-08-03 14:06:03 +0600

    lesson1 new component

--- lesson1/src/components/month/Month.js
+++ lesson1/src/components/month/Month.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Day from "../day/Day";
+import Time from "../time/Time";
 
 const Month = ({date}) => {
     return (
         <div>
             <p>Завтра будет {date}</p>
             <Day week_day={'четверг'}/>
+            <Time time_now={'14-05'}/>
         </div>
     );
 };
---
+++ lesson1/src/components/time/Time.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Time = ({time_now}) => {
+    return (
+        <div>
+            <p>Сейчас {time_now} </p>
+        </div>
+    );
+};
+
+export default Time;
