6f1919ddecc21fb9f0a3ffc51837d7a9bd2ec866
Kanykei <isakova0116@gmail.com>
2024-08-03 13:48:57 +0600

    lesson1

---
+++ lesson1/src/pages/mainpage/MainPage.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Header from "../../components/header/Header";
+import {Footer} from "../../components/footer/Footer";
+import Button from "../../components/button/Button";
+import User from "../../components/user/User";
+import Course from "../../components/course/Course";
+import Month from "../../components/month/Month";
+
+const MainPage = () => {
+    return (
+        <div>
+            <Header/>
+            <Footer/>
+            <Button title={'Close'} name={'Kanykei'}/>
+            <Button title={'Open'} name={'Kanykei'}/>
+            <Button title={'Save'} name={'Kanykei'}/>
+            <Button title={'Delete'} name={'Kanykei'}/>
+            <Button title={'Done'} name={'Kanykei'}/>
+            <User name={'Kanykei'} age={'20'}/>
+            <Course title={'JS'} language={'english'}/>
+            <Month date={'1'}/>
+        </div>
+    );
+};
+
+export default MainPage;
+
+const sum = (a,b) => {
+    return a+b
+}
+
+console.log(sum(5, 6))
